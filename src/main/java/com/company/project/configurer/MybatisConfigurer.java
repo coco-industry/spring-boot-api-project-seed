017167ac82b96981c8c97c1b48864a464be5b996
Gahon <Gahon1995@gmail.com>
2018-10-09 23:12:16 +0800

    调整通用Mapper设置

--- src/main/java/com/company/project/configurer/MybatisConfigurer.java
+++ src/main/java/com/company/project/configurer/MybatisConfigurer.java
@@ -1,19 +1,16 @@
 package com.company.project.configurer;
 
+
 import com.github.pagehelper.PageHelper;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 import tk.mybatis.spring.mapper.MapperScannerConfigurer;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.util.Properties;
 
@@ -21,6 +18,7 @@ import static com.company.project.core.ProjectConstant.*;
 
 /**
  * Mybatis & Mapper & PageHelper 配置
+ * @author Gahon
  */
 @Configuration
 public class MybatisConfigurer {
@@ -34,13 +32,16 @@ public class MybatisConfigurer {
         //配置分页插件，详情请查阅官方文档
         PageHelper pageHelper = new PageHelper();
         Properties properties = new Properties();
-        properties.setProperty("pageSizeZero", "true");//分页尺寸为0时查询所有纪录不再执行分页
-        properties.setProperty("reasonable", "true");//页码<=0 查询第一页，页码>=总页数查询最后一页
-        properties.setProperty("supportMethodsArguments", "true");//支持通过 Mapper 接口参数来传递分页参数
+        //分页尺寸为0时查询所有纪录不再执行分页
+        properties.setProperty("pageSizeZero", "true");
+        //页码<=0 查询第一页，页码>=总页数查询最后一页
+        properties.setProperty("reasonable", "true");
+        //支持通过 Mapper 接口参数来传递分页参数
+        properties.setProperty("supportMethodsArguments", "true");
         pageHelper.setProperties(properties);
 
         //添加插件
-        factory.setPlugins(new Interceptor[]{pageHelper});
+        factory.setPlugins(new Interceptor[]{(Interceptor) pageHelper});
 
         //添加XML目录
         ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
@@ -55,10 +56,16 @@ public class MybatisConfigurer {
         mapperScannerConfigurer.setBasePackage(MAPPER_PACKAGE);
 
         //配置通用Mapper，详情请查阅官方文档
+
         Properties properties = new Properties();
-        properties.setProperty("mappers", MAPPER_INTERFACE_REFERENCE);
-        properties.setProperty("notEmpty", "false");//insert、update是否判断字符串类型!='' 即 test="str != null"表达式内是否追加 and str != ''
+//        properties.setProperty("mappers", MAPPER_INTERFACE_REFERENCE); //4.0之后版本不需要，除非自定义mapper
+
+        //insert、update是否判断字符串类型!='' 即 test="str != null"表达式内是否追加 and str != ''
+        properties.setProperty("notEmpty", "false");
+        //取回主键的方式
         properties.setProperty("IDENTITY", "MYSQL");
+        //配置后会自动处理关键字，可以配的值和数据库有关。
+        properties.setProperty("wrapKeyword", "`{0}`");
         mapperScannerConfigurer.setProperties(properties);
 
         return mapperScannerConfigurer;
