884ca2a3e02d1c87c246796b850709fb91f39ebe
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-08-24 08:57:45 +0800

    update

---
+++ src/main/java/com/company/project/service/TestService.java
@@ -0,0 +1,12 @@
+package com.company.project.service;
+import com.company.project.model.Test;
+import com.company.project.core.Service;
+
+
+/**
+ * @author CodeGenerator
+ * @data 2021/08/24.
+ */
+public interface TestService extends Service<Test> {
+
+}
--- src/test/java/CodeGenerator.java
+++ src/test/java/CodeGenerator.java
@@ -18,9 +18,9 @@ import static com.company.project.core.ProjectConstant.*;
  */
 public class CodeGenerator {
     //JDBC配置，请修改为你项目的实际配置
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test_user"+ "?serverTimezone=GMT%2B8";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test"+ "?serverTimezone=UTC";
     private static final String JDBC_USERNAME = "root";
-    private static final String JDBC_PASSWORD = "123456";
+    private static final String JDBC_PASSWORD = "root";
     private static final String JDBC_DIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
 
     private static final String PROJECT_PATH = System.getProperty("user.dir");//项目在硬盘上的基础路径
@@ -37,8 +37,9 @@ public class CodeGenerator {
     private static final String DATE = new SimpleDateFormat("yyyy/MM/dd").format(new Date());//@date
 
     public static void main(String[] args) {
-        genCode("user");
-        //genCodeByCustomModelName("输入表名","输入自定义Model名称");
+        // genCode("输入表名");
+        // genCodeByCustomModelName("输入表名","输入自定义Model名称");
+        genCode("test");
     }
 
     /**
